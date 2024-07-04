9abe6a27f502cfed0daf37c5023dd87bbd8ab0a4
totemosouomou <totemosouomou@users.noreply.github.com>
2024-07-04 21:06:09 +0900

    GitHub Pages：portfolio

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 // src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App.tsx';
 import './App.scss';
 import './index.css';
 
