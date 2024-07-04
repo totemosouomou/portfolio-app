b7d7979fc93a7139e406678fd41fb31c8c8b5696
totemosouomou <totemosouomou@users.noreply.github.com>
2024-07-04 20:55:00 +0900

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES  // この行を追加
+  ? "portfolio-app"               // この行を追加
+  : "./",                         // この行を追加
   plugins: [react()],
 })
